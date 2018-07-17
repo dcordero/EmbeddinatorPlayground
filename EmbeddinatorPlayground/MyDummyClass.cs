0bf5b47b0a122ac1f25afbf49afc1ef52b0274e1
David Cordero <david@corderoramirez.com>
2018-07-17 10:58:26 +0200

    Create dummy .net framework which uses System.Net.Http

---
+++ EmbeddinatorPlayground/MyDummyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace EmbeddinatorPlayground
+{
+    public class MyDummyClass
+    {
+        HttpClient _client;
+
+        public MyDummyClass(HttpClient client = null)
+        {
+            _client = client == null ? new HttpClient() : null;
+        }
+
+        public async void FetchData()
+        {
+            HttpResponseMessage response = await _client.GetAsync("https://jsonplaceholder.typicode.com/users");
+            Console.WriteLine(response.IsSuccessStatusCode ? "OK" : "KO");
+        }
+    }
+}
